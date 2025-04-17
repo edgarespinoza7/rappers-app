8bfcf1ccd36e9aacb9a0c7bebba9816644de7ca9
edgarespinoza7 <edgarespinoza7@gmail.com>
2025-04-17 22:54:53 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,41 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
+    <div className="p-6">
+      {/* Navigation */}
+      <nav className="flex items-center justify-between">
+        <span className="font-bold text-2xl ">50</span>
+        <ul className="flex space-x-4 text-sm ">
           <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+            <Link href="/">Home</Link>
           </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+     <ModeToggle />
+      </nav>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <header className="flex flex-col items-center justify-center mt-8 h-[30vh] bg-gradient-to-b from-gray-500/10 to-gray-700/20 rounded-lg shadow-lg p-8">
+        <div>
+          <h1 className="text-4xl text-center mt-8 font-bold">
+            50 grandes en la historia del rap en español
+          </h1>
+          <p className="text-center mt-4">
+            Un reconocimiento a las figuras más importantes del género en
+            nuestro idioma
+          </p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex flex-col items-center">
+          <Button className="mt-8">Get Started</Button>
+        </div>
+      </header>
     </div>
   );
 }
